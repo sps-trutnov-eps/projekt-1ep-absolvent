2899f3ce00c18948b81680414989452dcb3ef86f
Alexander Hamal <hamala24@spstrutnov.cz>
2025-04-10 17:03:39 +0200

    jen test na velikost okna

---
+++ looting kose/grid.py
@@ -0,0 +1,7 @@
+import pygame
+import random
+from item import Itemy
+
+#itemy = bobek, ohryzek, kebab, noviny, lahev, krabicak, hodinky, tuzemak, energetak, derava_cepice, derave_tricko, derave_kalhoty, pizza, burger
+
+#item choosing system
