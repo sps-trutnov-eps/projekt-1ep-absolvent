fc4900b468167b7e402077035d4f26bcb2079d8f
Alexander Hamal <hamala24@spstrutnov.cz>
2025-04-12 22:34:11 +0200

    jen hokusy pokusy, nic důležitého

--- looting kose/grid.py
+++ looting kose/grid.py
@@ -1,24 +1,9 @@
 import pygame
 import random
+import sys
 from itemy import Itemy
 
 #itemy = bobek, ohryzek, kebab, noviny, lahev, krabicak, hodinky, tuzemak, energetak, derava_cepice, derave_tricko, derave_kalhoty, pizza, burger
-
-#textury
-textura_bobku = pygame.image.load("looting kose//textury//bobek.png")
-textura_ohryzku = pygame.image.load("looting kose//textury//ohryzek.png")
-textura_kebabu = pygame.image.load("looting kose//textury//kebab.png")
-textura_novin = pygame.image.load("looting kose//textury//noviny.png")
-textura_lahve = pygame.image.load("looting kose//textury//lahev.png")
-textura_krabicaku = pygame.image.load("looting kose//textury//krabicak.png")
-textura_hodinek = pygame.image.load("looting kose//textury//hodinky.png")
-textura_tuzemaku = pygame.image.load("looting kose//textury//tuzemak.png")
-textura_energetaku = pygame.image.load("looting kose//textury//energetak.png")
-textura_derave_cepice = pygame.image.load("looting kose//textury//cepice.png")
-textura_derave_tricko = pygame.image.load("looting kose//textury//tricko.png")
-textura_derave_kalhoty = pygame.image.load("looting kose//textury//kalhoty.png")
-textura_pizza = pygame.image.load("looting kose//textury//pizza.png")
-textura_burger = pygame.image.load("looting kose//textury//burger.png")
 
 #okno
 okraje = 20
@@ -29,7 +14,7 @@ rozliseni_y = rozliseni_x
 okno = pygame.display.set_mode((rozliseni_x, rozliseni_y))
 
 #class aktivace
-item = Itemy(okno, textura_bobku, textura_ohryzku, textura_kebabu, textura_novin, textura_lahve, textura_krabicaku, textura_hodinek, textura_tuzemaku, textura_energetaku, textura_derave_cepice, textura_derave_tricko, textura_derave_kalhoty, textura_pizza, textura_burger)
+item = Itemy(okno)
 
 #item choosing system
 itemy = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -47,3 +32,86 @@ while vybrane_itemy < 9:
 print(itemy_v_kosi)
 
 #priradit cislum itemy
+real_itemy = {
+    0: item.bobek,
+    1: item.ohryzek,
+    2: item.kebab,
+    3: item.noviny,
+    4: item.lahev,
+    5: item.krabicak,
+    6: item.hodinky,
+    7: item.tuzemak,
+    8: item.energetak,
+    9: item.cepice,
+    10: item.tricko,
+    11: item.kalhoty,
+    12: item.pizza,
+    13: item.burger
+}
+
+#pozice radek a sloupec
+cerna = (0, 0, 0)
+bila = (255, 255, 255)
+
+#grid properties
+velikost_ctverecku = 93 + 1/3
+gap = 10
+velikost_gridu = 3
+
+row_1 = okraje
+row_2 = okraje + velikost_ctverecku + gap
+row_3 = okraje + 2 * velikost_ctverecku + 2 * gap
+
+col_1 = row_1
+col_2 = row_2
+col_3 = row_3
+
+while True:
+    lmb = False
+
+    udalosti = pygame.event.get()
+    for udalost in udalosti:
+        if udalost.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if udalost.type == pygame.MOUSEBUTTONUP and udalost.button == 1:
+            lmb = True
+
+    mys = pygame.mouse.get_pos()
+
+    #print(lmb)
+
+    okno.fill(bila)
+
+    slot_1_r = pygame.Rect(row_1, col_1, velikost_ctverecku, velikost_ctverecku)
+    slot_2_r = pygame.Rect(row_1, col_2, velikost_ctverecku, velikost_ctverecku)
+    slot_3_r = pygame.Rect(row_1, col_3, velikost_ctverecku, velikost_ctverecku)
+
+    slot_4_r = pygame.Rect(row_2, col_1, velikost_ctverecku, velikost_ctverecku)
+    slot_5_r = pygame.Rect(row_2, col_2, velikost_ctverecku, velikost_ctverecku)
+    slot_6_r = pygame.Rect(row_2, col_3, velikost_ctverecku, velikost_ctverecku)
+    
+    slot_7_r = pygame.Rect(row_3, col_1, velikost_ctverecku, velikost_ctverecku)
+    slot_8_r = pygame.Rect(row_3, col_1, velikost_ctverecku, velikost_ctverecku)
+    slot_9_r = pygame.Rect(row_3, col_3, velikost_ctverecku, velikost_ctverecku)
+
+    if not slot_1_r.collidepoint(mys) and not lmb:
+        slot_1 = pygame.draw.rect(okno, cerna, slot_1_r)
+    if not slot_2_r.collidepoint(mys) and not lmb:
+        slot_2 = pygame.draw.rect(okno, cerna, slot_3_r)
+    if not slot_3_r.collidepoint(mys) and not lmb:
+        slot_3 = pygame.draw.rect(okno, cerna, slot_3_r)
+
+    slot_4 = pygame.draw.rect(okno, cerna, slot_4_r)
+    slot_5 = pygame.draw.rect(okno, cerna, slot_5_r)
+    slot_6 = pygame.draw.rect(okno, cerna, slot_6_r)
+
+    slot_7 = pygame.draw.rect(okno, cerna, slot_8_r)
+    slot_8 = pygame.draw.rect(okno, cerna, slot_8_r)
+    slot_9 = pygame.draw.rect(okno, cerna, slot_9_r)
+
+    print(row_2, col_1)
+    print(row_2, col_3)
+
+    pygame.display.flip()
--- looting kose/itemy.py
+++ looting kose/itemy.py
@@ -1,74 +1,72 @@
 import pygame
 pygame.init()
 class Itemy:
-        def __init__(self, okno, textura_bobku, textura_ohryzku, textura_kebabu, textura_novin, textura_lahve, textura_krabicaku, textura_hodinek, textura_tuzemaku, textura_energetaku, textura_derava_cepice, textura_derave_tricko
-                        , textura_derave_kalhoty, textura_pizzy, textura_burger):
-        
+        def __init__(self, okno):
                 self.okno = okno
 
-                self.textura_bobku = textura_bobku
-                self.textura_ohryzku = textura_ohryzku
-                self.textura_kebabu = textura_kebabu        
-                self.textura_novin = textura_novin
-                self.textura_lahve = textura_lahve
-                self.textura_krabicaku = textura_krabicaku
-                self.textura_hodinek = textura_hodinek
-                self.textura_tuzemaku = textura_tuzemaku
-                self.textura_energetaku = textura_energetaku
-                self.textura_derava_cepice = textura_derava_cepice
-                self.textura_derave_tricko = textura_derave_tricko
-                self.textura_derave_kalhoty = textura_derave_kalhoty
-                self.textura_pizzy = textura_pizzy
-                self.textura_burger = textura_burger
-
+                self.textura_bobku = pygame.image.load("looting kose//textury//bobek.png")
+                self.textura_ohryzku = pygame.image.load("looting kose//textury//ohryzek.png")
+                self.textura_kebabu = pygame.image.load("looting kose//textury//kebab.png")
+                self.textura_novin = pygame.image.load("looting kose//textury//noviny.png")
+                self.textura_lahve = pygame.image.load("looting kose//textury//lahev.png")
+                self.textura_krabicaku = pygame.image.load("looting kose//textury//krabicak.png")
+                self.textura_hodinek = pygame.image.load("looting kose//textury//hodinky.png")
+                self.textura_tuzemaku = pygame.image.load("looting kose//textury//tuzemak.png")
+                self.textura_energetaku = pygame.image.load("looting kose//textury//energetak.png")
+                self.textura_derave_cepice = pygame.image.load("looting kose//textury//cepice.png")
+                self.textura_derave_tricko = pygame.image.load("looting kose//textury//tricko.png")
+                self.textura_derave_kalhoty = pygame.image.load("looting kose//textury//kalhoty.png")
+                self.textura_pizza = pygame.image.load("looting kose//textury//pizza.png")
+                self.textura_burger = pygame.image.load("looting kose//textury//burger.png")
+        
         def bobek(self, x, y):
-                self.okno.blit(self.textura_bobku, x, y)
-                self.time_debuffu = 1500
+                self.okno.blit(self.textura_bobku, (x, y))
+                self.time_debuffu = 150
 
         def ohryzek(self, x, y):
-                self.okno.blit(self.textura_ohryzku, x, y)
+                self.okno.blit(self.textura_ohryzku, (x, y))
                 
                 #kdyz uzito:
                 self.food_stat = 12
 
         def kebab(self, x, y):
-                self.okno.blit(self.textura_kebabu, x, y)
+                self.okno.blit(self.textura_kebabu, (x, y))
                 
                 #kdyz uzito:
                 self.food_stat = 20
 
         def noviny(self, x, y):
-                self.okno.blit(self.textura_novin, x, y)
+                self.okno.blit(self.textura_novin, (x, y))
 
                 #kdyz uzito:
-                self.burn_time = 1500
+                self.burn_time= 1500
 
         def lahev(self, x, y):
-                self.okno.blit(self.textura_lahve, x, y)
+                self.okno.blit(self.textura_lahve, (x, y))
 
                 #kdyz uzito:
-                self.cena = 1
+                self.cena = 10
 
         def krabicak(self, x, y):
-                self.okno.blit(self.textura_krabicaku, x, y)
+                self.okno.blit(self.textura_krabicaku, (x, y))
                 
                 #kdyz uzito:
-                self.alko_teplo = 10
+                self.alko_tepo = 10
 
         def hodinky(self, x, y):
-                self.okno.blit(self.textura_hodinek, x, y)
+                self.okno.blit(self.textura_hodinek, (x, y))
 
                 #kdyz uzito:
-                self.cena = 25
+                self.cena = 50
 
         def tuzemak(self, x, y):
-                self.okno.blit(self.textura_tuzemaku, x, y)
+                self.okno.blit(self.textura_tuzemaku, (x, y))
 
                 #kdyz uzito
-                self.alko_teplo = 20
+                self.alko_tepo = 20
 
         def energetak(self, x, y):
-                self.okno.blit(self.textura_energetaku, x, y)
+                self.okno.blit(self.textura_energetaku, (x, y))
 
                 #kdyz uzito:
                 self.speed_boost = 5
@@ -83,13 +81,13 @@ class Itemy:
                 self.okno.blit(self.textura_derave_kalhoty, x, y)
 
         def pizza(self, x, y):
-                self.okno.blit(self.textura_pizzy, x, y)
+                self.okno.blit(self.textura_pizzy, (x, y))
 
                 #kdyz uzito:
-                self.food_stat = 21
+                self.food_stat = 15
 
         def burger(self, x, y):
-                self.okno.blit(self.textura_burger, x, y)
+                self.okno.blit(self.textura_burger, (x, y))
 
                 #kdyz uzito:
                 self.food_stat = 25
