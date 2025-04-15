eb8c10a74599cc84f001a54c188843c814cbc285
Jiří Krudenc <krudencj24@spstrutnov.cz>
2025-04-15 10:36:42 +0200

    Kompletní změna podle šablony
    !Nefunguje!

---
+++ source/sablona.py
@@ -0,0 +1,13 @@
+import main as master
+from main import main as masterFunc
+from main import convertFuncToStr as novyProgram
+
+# sem piste importy
+
+def main(global_data):
+    pass
+    # pro otevreni okna "global_data['otevrena_okna'].append(novyProgram(funkce))"
+    # sem piste svuj program
+
+if __name__ == "__main__":
+    masterFunc(novyProgram(main))
