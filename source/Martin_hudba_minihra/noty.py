250d9a9fa69c326bf8159eec359ceb4daac4474d
Martin Jirouš <jirousm24@spstrutnov.cz>
2025-04-15 10:35:19 +0200

    příprava not

--- source/Martin_hudba_minihra/noty.py
+++ source/Martin_hudba_minihra/noty.py
@@ -1,7 +1,20 @@
 import pygame
 
 class nota:
-    def __init__(self,poloha_x,poloha_y):
+    def __init__(self,šířka):
+        
         self.poloha_x=poloha_x
-        self.poloha_y=poloha_y
-    
+        self.poloha_y=0
+        self.ktera_nota = 1
+        self.aktivní = 0
+        
+    def cas_na_start(self,hudba,kdy):
+        
+        if hudba[self.ktera_nota][kdy]:
+            self.aktivní = 1
+            self.poloha_y=kde_start*50
+            
+    def pohyb(self):
+        
+        if self.aktivní ==1:
+            self.pohyb_x-=10
