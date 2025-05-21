ddd3da548af8e48211ab4fbd7fd0814acb018c8f
Jakub Blažík <blazikj24@spstrutnov.cz>
2025-05-21 20:24:51 +0200

    přesunutí hlavního kódu do funkce main

--- source/kasna/kasna.py
+++ source/kasna/kasna.py
@@ -1,13 +1,11 @@
 import pygame
 import random
 from mince import Mince, spawn, nastav_sance, ziskej_sance
-penize = 0
 
-# Inicializace
+
 pygame.init()
 width, height = 800, 500
 screen = pygame.display.set_mode((width, height))
-menu_screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Kasna s mincemi")
 clock = pygame.time.Clock()
 running = True
@@ -15,37 +13,42 @@ font = pygame.font.SysFont("Arial", 25)
 font_velky = pygame.font.SysFont("Arial", 32)
 pocet_minci = random.randint(0, 8)
 mince = spawn(pocet_minci) 
-kasna=pygame.image.load("kasna.png").convert_alpha()
-zem=pygame.image.load("zem.png").convert_alpha()
+penize = 0
 
-while running:
+
+kasna = pygame.image.load("kasna.png").convert_alpha()
+zem = pygame.image.load("zem.png").convert_alpha()
+
+
+def main():
+    global running, penize  
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # Získání pozice myši
+
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pozice_mys = pygame.mouse.get_pos()
-                
-            # Kontrola kliknutí na každou minci
-            for minci in mince[:]:  
+
+            for minci in mince[:]: 
                 if minci.je_kliknuto(pozice_mys[0], pozice_mys[1]):
                     mince.remove(minci)  
                     penize += minci.hodnota
                     break  
-    
 
-    screen.blit(zem,(0,0))
+    screen.blit(zem, (0, 0))
     pygame.draw.ellipse(screen, "blue", (-1, 2.5, 800, 500))
     
-    screen.blit(kasna,(0, 0))
+    screen.blit(kasna, (0, 0))
     text = font.render(f"{penize} Kč", True, "white")
     screen.blit(text, (20, 20))
-    
-    # Vykreslovani minci
+
     for minci in mince:
         minci.vykresli_se(screen)
-
+    
     pygame.display.flip()
     clock.tick(60)
 
+while running:
+    main()
 pygame.quit()
