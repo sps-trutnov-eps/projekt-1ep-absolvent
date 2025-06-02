860e1c3df8ebb18098583fc4bc4531b32204abe9
Martin Jirouš <jirousm24@spstrutnov.cz>
2025-06-02 09:35:34 +0200

    nový shop

---
+++ source/shop2/button.py
@@ -0,0 +1,16 @@
+import pygame
+class Button :
+    def __init__(self,x,y,textura):
+        self.x=x
+        self.y=y
+        self.textura=textura
+        self.rect=pygame.Rect(self.x, self.y, self.textura.get_width(), self.textura.get_height())
+    
+    def draw(self,okno):
+        okno.blit(self.textura,(self.rect))
+    
+    def collide(self,pos):
+        return self.rect.collidepoint(pos[0],pos[1])
+        
+        
+    
---
+++ source/shop2/main.py
@@ -0,0 +1,75 @@
+import pygame
+import sys 
+from button import Button
+
+textury = [
+    {
+        "button_1_1":pygame.image.load("textury/batoh1.png"),
+        "button_1_2":pygame.image.load("textury/batoh2.png"),
+        "button_1_3":pygame.image.load("textury/batoh3.png"),
+        "button_1_4":pygame.image.load("textury/batoh4.png"),
+    },
+    {
+        "button_2_1":pygame.image.load("textury/pití1.png"),
+        "button_2_2":pygame.image.load("textury/pití2.png"),
+        "button_2_3":pygame.image.load("textury/pití3.png"),
+        "button_2_4":pygame.image.load("textury/pití4.png"),
+    },
+    {
+        "preview_1_1":pygame.transform.scale(pygame.image.load("textury/batoh1.png"),(100,100)),
+        "preview_1_2":pygame.transform.scale(pygame.image.load("textury/batoh2.png"),(100,100)),
+        "preview_1_3":pygame.transform.scale(pygame.image.load("textury/batoh3.png"),(100,100)),
+        "preview_1_4":pygame.transform.scale(pygame.image.load("textury/batoh4.png"),(100,100)),
+    },
+    {
+        "preview_2_1":pygame.transform.scale(pygame.image.load("textury/pití1.png"),(100,100)),
+        "preview_2_2":pygame.transform.scale(pygame.image.load("textury/pití2.png"),(100,100)),
+        "preview_2_3":pygame.transform.scale(pygame.image.load("textury/pití3.png"),(100,100)),
+        "preview_2_4":pygame.transform.scale(pygame.image.load("textury/pití4.png"),(100,100)),
+    },
+    {
+        "selection_button_1":pygame.image.load("textury/Button_back.png"),
+        "selection_button_2":pygame.image.load("textury/Button_back.png"),
+    }
+]
+s
+textury[0]["button_1_1"]
+
+backround=pygame.image.load("textury/pozadí_shopu.png")
+
+shop = True 
+x,y = 800,600
+buttony = []
+for j, i in enumerate(textury):
+    buttony.append(Button(100, j*100, textury[i] ))
+    
+
+
+screen = pygame.display.set_mode((x, y))
+pygame.display.set_caption("Cigansky obchod")
+
+while shop:
+    mouse_pos = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            shop = False
+
+    screen.blit(backround, (0, 0))
+    for i in buttony:
+        i.draw(screen)
+        if i.collide(mouse_pos):
+            if 
+    
+
+
+
+
+
+
+
+
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
